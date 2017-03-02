67647000e24bfad9f28e95c57727e2940760a3f6
Brian Sawyer <bsawyer@basistech.com>
2017-03-02 18:25:38 -0500

    WS-1086 Wiring docker IT test into maven build
    The docker maven plugin is now used to install elastic with our plugin,
    launch it, and then (via shell scripts) run some commands and test
    for any errors.  This happens as part of the standard maven build.

--- docker/README.md
+++ docker/README.md
@@ -4,9 +4,19 @@ Testing the plugin
 `mvn clean install` from the top level directory first, then:
 
 ```
-./build.sh
-ROSETTE_API_KEY=<blah> ./run.sh
+mvn docker:build
+ROSETTE_API_KEY=<blah> mvn docker:run
+
 <wait for it to fully start>
-./test.sh
-<tweak test.sh to add index and query tests as you wish>
+<look in the logs for the local port docker chose to map to (ie. 'Waiting on url http://localhost:32769')
+<pass that port as an argument to the test script>
+
+./test.sh <docker_port>
 ```
+==================
+
+There is a known issue where the docker container logs an error message for unknown reasons:
+```
+[ERROR] DOCKER> Cannot process chunk response: java.io.IOException: Bad file descriptor
+```
+This can be ignored.
--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -XPUT 'localhost:9200/_ingest/pipeline/rosapi?pretty' -d'
+# uses '|| true' to make sure that even if there is an error,
+# the maven build continues and stops the docker image
+curl -XPUT "localhost:$1/_ingest/pipeline/rosapi?pretty" -d'
 {
   "processors": [
     {
@@ -8,12 +10,98 @@ curl -XPUT 'localhost:9200/_ingest/pipeline/rosapi?pretty' -d'
         "field" : "text",
         "target_field" : "language"
       }
+    },
+    {
+      "ros_categories" : {
+        "field" : "text",
+        "target_field" : "category"
+      }
+    },
+    {
+      "ros_sentiment" : {
+        "field" : "text",
+        "target_field" : "sentiment"
+      }
+    },
+    {
+      "ros_entities" : {
+        "field" : "text",
+        "target_field" : "entities_sentiment",
+        "include_sentiment" : true,
+        "include_offsets" : true,
+        "include_translation" : true,
+        "translation_language" : "eng"
+      }
+    },
+    {
+      "ros_entities" : {
+        "field" : "text",
+        "target_field" : "entities",
+        "include_sentiment" : false,
+        "include_offsets" : false,
+        "include_translation" : false,
+        "translation_language" : "eng"
+      }
+    },
+    {
+      "ros_name_translation" : {
+        "field" : "name",
+        "target_field" : "translation",
+        "target_language" : "rus"
+      }
     }
   ]
 }
-'
+' || true
 
-curl -XPUT 'localhost:9200/test_idx?pretty' -d'
+#Pipeline without categories since it only supports English
+curl -XPUT "localhost:$1/_ingest/pipeline/rosapi_jpn?pretty" -d'
+{
+  "processors": [
+    {
+      "ros_language" : {
+        "field" : "text",
+        "target_field" : "language"
+      }
+    },
+    {
+      "ros_sentiment" : {
+        "field" : "text",
+        "target_field" : "sentiment"
+      }
+    },
+    {
+      "ros_entities" : {
+        "field" : "text",
+        "target_field" : "entities_sentiment",
+        "include_sentiment" : true,
+        "include_offsets" : true,
+        "include_translation" : true,
+        "translation_language" : "eng"
+      }
+    },
+    {
+      "ros_entities" : {
+        "field" : "text",
+        "target_field" : "entities",
+        "include_sentiment" : false,
+        "include_offsets" : false,
+        "include_translation" : false,
+        "translation_language" : "eng"
+      }
+    },
+    {
+      "ros_name_translation" : {
+        "field" : "name",
+        "target_field" : "translation",
+        "target_language" : "rus"
+      }
+    }
+  ]
+}
+' || true
+
+curl -XPUT "localhost:$1/test_idx?pretty" -d'
 {
   "mappings": {
     "rosette": {
@@ -24,54 +112,29 @@ curl -XPUT 'localhost:9200/test_idx?pretty' -d'
     }
   }
 }
-'
+' || true
 
-curl -XPUT 'localhost:9200/test_idx/rosette/1?pretty&refresh=true&pipeline=rosapi' -d'
+curl -XPUT "localhost:$1/test_idx/rosette/1?pretty&refresh=true&pipeline=rosapi" -d'
 {
   "text": "I know that I know nothing"
 }
-'
+' || true
 
-curl -XPUT 'localhost:9200/test_idx/rosette/2?pretty&refresh=true&pipeline=rosapi' -d'
+curl -XPUT "localhost:$1/test_idx/rosette/2?pretty&refresh=true&pipeline=rosapi_jpn" -d'
 {
   "text": "ξέρω ότι δεν ξέρω τίποτα"
 }
-'
+' || true
 
-curl -XPUT 'localhost:9200/test_idx/rosette/3?pretty&refresh=true&pipeline=rosapi' -d'
+curl -XPUT "localhost:$1/test_idx/rosette/3?pretty&refresh=true&pipeline=rosapi" -d'
 {
   "text": "No one desires evil"
 }
-'
+' || true
 
 sleep 3
 
-#curl -XPOST 'localhost:9200/test_idx/_search?pretty' -d'
-#{
-#  "query": {
-#    "match" : {
-#      "content.iso639" : "eng"
-#    }
-#  }
-#}
-#'
-
-#curl -XPOST 'localhost:9200/test_idx/_search?pretty' -d'
-#{
-#  "query": {
-#    "bool" : {
-#      "must" : {
-#        "term" : { "content" : "know" }
-#      },
-#      "must" : {
-#        "match": {"content.iso639" : "ell"}
-#      }
-#    }
-#  }
-#}
-#'
-
-curl -XPOST 'localhost:9200/test_idx/_search?pretty' -d'
+curl -XPOST "localhost:$1/test_idx/_search?pretty" -d'
 {
   "query": {
     "constant_score" : {
@@ -81,4 +144,4 @@ curl -XPOST 'localhost:9200/test_idx/_search?pretty' -d'
     }
   }
 }
-'
+' || true
