8611d70b77d9ced07a0ba8ad98a86e499dcdb150
Joe Ho <jkho@basistech.com>
2020-07-21 17:10:13 -0400

    Debug travis ci failure

--- docker/verify.sh
+++ docker/verify.sh
@@ -2,10 +2,12 @@
 
 if [[ "$(tail -1 "$1" 2>/dev/null)" !=  "exit: 0" ]] ; then
     echo "There are test failures. Exiting..."
+    cat $1
     exit 1
 fi
 
 if grep -E -i 'exception|"failed":[1-9]' "$1" ; then
     echo "Test query failed! See $1 for details. Exiting..."
+    cat $1
     exit 1
 fi
