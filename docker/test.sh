baf418841abc9fbf9b9b3bcaa244b9ad9d4d63de
Seth Gransky <seth@basistech.com>
2020-07-28 16:51:59 -0500

    Try for more insight into our sproadic failures.

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 code=0
+command_counter=0
 set -x
 
 # uses '|| code=$?' to make sure that even if there is an error,
@@ -19,12 +20,16 @@ curl -fsSL -H 'Content-Type: application/json' -XPUT "$1:$2/_ingest/pipeline/my_
   ]
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H 'Content-Type: application/json' -XPOST "$1:$2/indexname/mappingName?pipeline=my_pipeline&pretty" -d'
 {
   "text" : "This is a document containing English text"
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/_ingest/pipeline/rosapi?pretty" -d'
 {
@@ -77,6 +82,8 @@ curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/_ingest/pipeline/ros
   ]
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 #Pipeline without categories since it only supports English
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/_ingest/pipeline/rosapi_jpn?pretty" -d'
@@ -124,6 +131,8 @@ curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/_ingest/pipeline/ros
   ]
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx?include_type_name=true&pretty" -d'
 {
@@ -142,18 +151,24 @@ curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx?include_typ
   }
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx/rosette/1?pretty&refresh=true&pipeline=rosapi" -d'
 {
   "text": "Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, “The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.”"
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx/rosette/2?pretty&refresh=true&pipeline=rosapi_jpn" -d'
 {
   "text": "バングラデシュ政府、ロヒンギャ難民の島への移動を計画 \nバングラデシュ政府、ロヒンギャ難民の島への移動を計画\n\nテンガール・チャール島は約10年前に、メグナ川の堆積土で形成され、高潮の際には数十センチの水に囲まれてしまう。道路や堤防などは築かれておらず、島を記載する地図はあまりない。\n\n約30キロ西には60万人が住むハティｱ島があり、現在の難民キャンプからの移動には9時間かかる。\n\nある地元政府関係者はAFP通信に対し、テンガール・チャール島について、「島に行けるのは冬のみで、海賊たちの隠れ家になっている」と語った。島を洪水から守るため植樹が行われているが、完了するまでには少なくとも10年がかかるという。同関係者は、「モンスーンの季節には完全に水浸しになってしまう」と話し、「あそこに住まわせるというのは、ひどいアイデアだ」と指摘した。\n\nImage caption 移住が計画されているテンガール・チャール島はハティア（Hatiya）島の近くにある\n\nミャンマーでは、ロヒンギャの人々は国境を接するバングラデシュからの不法移民として扱われており、国籍の取得ができずにいる。\n\n"
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx/rosette/3?pretty&refresh=true&pipeline=rosapi" -d'
 {
@@ -161,6 +176,8 @@ curl -fsSL -H "Content-Type: application/json" -XPUT "$1:$2/test_idx/rosette/3?p
   "name" : "Vladimir Nabokov"
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 sleep 3
 
@@ -175,6 +192,8 @@ curl -fsSL -H "Content-Type: application/json" -XPOST "$1:$2/test_idx/_search?pr
   }
 }
 ' || ((code++))
+echo "After command ${command_counter} our error code count is ${code}."
+command_counter=$((command_counter + 1))
 
 set +x
 echo "exit: $code"
